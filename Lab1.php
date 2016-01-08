2885b6cb218a98eba2fd465ba46c16ce880114d5
Rin <Rin@Rin-HP>
2016-01-07 19:57:16 -0800

    Step Two

--- Lab1.php
+++ Lab1.php
@@ -13,7 +13,10 @@ and open the template in the editor.
         <?php
         $position = $_GET['board'];
         $squares = str_split($position);
-        if (winner('x', $squares)) {
+        if (isset($GET['board'])){
+            echo 'Please set a board';
+        }
+        else if (winner('x', $squares)) {
             echo 'You win.';
         } else if (winner('o', $squares)) {
             echo 'I win.';
@@ -25,41 +28,16 @@ and open the template in the editor.
     <?php
 
     function winner($token, $position) {
-        $won = false;
-        if (($position[0] == $token) &&
-                ($position[1] == $token) &&
-                ($position[2] == $token)) {
-            $won = true;
-        } else if (($position[3] == $token) &&
-                ($position[4] == $token) &&
-                ($position[5] == $token)) {
-            $won = true;
-        } else if (($position[6] == $token) &&
-                ($position[7] == $token) &&
-                ($position[8] == $token)) {
-            $won = true;
-        } else if (($position[0] == $token) &&
-                ($position[3] == $token) &&
-                ($position[6] == $token)) {
-            $won = true;
-        } else if (($position[1] == $token) &&
-                ($position[4] == $token) &&
-                ($position[7] == $token)) {
-            $won = true;
-        } else if (($position[2] == $token) &&
-                ($position[5] == $token) &&
-                ($position[8] == $token)) {
-            $won = true;
-        } else if (($position[0] == $token) &&
-                ($position[4] == $token) &&
-                ($position[8] == $token)) {
-            $won = true;
-        } else if (($position[2] == $token) &&
-                ($position[4] == $token) &&
-                ($position[6] == $token)) {
-            $won = true;
+        $won = True;
+        for($row=0; $row<3; $row++) {
+   $result = true;
+   for ($col = 0; $col < 3; $col++) {
+       if ($position[3 * $row + $col] != $token) {
+                $result = false;
+            }
         }
-        return $won;
+    }
+    return $won;
     }
     ?>
 </html>
