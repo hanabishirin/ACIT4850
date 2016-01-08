bb22af54c9b14f3cb51c3346f7d762ef33170b86
Rin <Rin@Rin-HP>
2016-01-08 09:29:55 -0800

    Step Two

--- Lab1.php
+++ Lab1.php
@@ -12,32 +12,56 @@ and open the template in the editor.
     <body>
         <?php
         $position = $_GET['board'];
-        $squares = str_split($position);
-        if (isset($GET['board'])){
-            echo 'Please set a board';
-        }
-        else if (winner('x', $squares)) {
-            echo 'You win.';
-        } else if (winner('o', $squares)) {
-            echo 'I win.';
+        $squares = $position;
+
+        $game = new Game($squares);
+        $game->display();
+        if ($game -> winner('x')) {
+            echo 'You win. Lucky guesses!';
+        } else if ($game -> winner('o')) {
+            echo 'I win. Muahahahaha';
         } else {
-            echo 'No winner yet.';
+            echo 'No winner yet, but you are losing.';
         }
         ?>
     </body>
     <?php
 
-    function winner($token, $position) {
-        $won = True;
-        for($row=0; $row<3; $row++) {
-   $result = true;
-   for ($col = 0; $col < 3; $col++) {
-       if ($position[3 * $row + $col] != $token) {
-                $result = false;
+    class Game {
+
+        var $position;
+
+        function display() {
+            echo '<table cols=”3” style=”font­size:large; font­weight:bold”>';
+            echo '<tr>'; // open the first row
+            for ($pos = 0; $pos < 9; $pos++) {
+                echo '<td>-</td>';
+                if ($pos % 3 == 2) {
+                    echo '</tr><tr>';
+                } // start a new row for the next square
             }
+            echo '</tr>'; // close the last row
+            echo '</table>';
         }
-    }
-    return $won;
+
+        function __construct($squares) {
+            $this->position = str_split($squares);
+        }
+
+        function winner($token) {
+            for ($row = 0; $row < 3; $row++) {
+                $result = true;
+                for ($col = 0; $col < 3; $col++) {
+                    if ($this->position[3 * $row + $col] == $token) {
+                        $result = false;
+                    }
+                    if ($result == true) {
+                        return $result;
+                    }
+                }
+            }
+            return $result;
+        }
     }
     ?>
 </html>
